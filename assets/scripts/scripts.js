6a5a94d713ba0732758abb8a9610020b5310893b
dorinengabdoh <ngabdohdorine@gmail.com>
2023-10-16 12:48:55 -0400

    fix lints

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,44 +1,39 @@
-const amountInput = document.getElementById("amount");
-const fromCurrencySelect = document.getElementById("from");
-const toCurrencySelect = document.getElementById("to");
-const convertedAmount = document.getElementById("converted-amount");
-let convert;
+const amountInput = document.getElementById('amount')
+const fromCurrencySelect = document.getElementById('from')
+const toCurrencySelect = document.getElementById('to')
+const convertedAmount = document.getElementById('converted-amount')
 
-amountInput.addEventListener("input", updateConversion);
-fromCurrencySelect.addEventListener("change", updateConversion);
-toCurrencySelect.addEventListener("change", updateConversion);
-convertedAmount.addEventListener("input", () => {
-  const amount2 = parseFloat(convertedAmount.value) ||0;
-  const exchangeRate2 = exchangeRates[toCurrencySelect.value] / exchangeRates[fromCurrencySelect.value];
-  const convert2 = (amount2 * exchangeRate2).toFixed(2);
-  return amountInput.value = convert2,
-  // console.log(convert2),
-  updateConversion();
-});
+amountInput.addEventListener('input', updateConversion)
+fromCurrencySelect.addEventListener('change', updateConversion)
+toCurrencySelect.addEventListener('change', updateConversion)
+convertedAmount.addEventListener('input', () => {
+  const amount2 = parseFloat(convertedAmount.value) || 0
+  const exchangeRate2 = exchangeRates[toCurrencySelect.value] / exchangeRates[fromCurrencySelect.value]
+  const convert2 = (amount2 * exchangeRate2).toFixed(2)
+  amountInput.value = convert2
+  return convert2,
+  updateConversion()
+})
 
 const exchangeRates = {
   usd: 1,
   eur: 0.95,
   gbp: 0.72,
-  yen: 149.4,
-};
+  yen: 149.4
+}
 
-
-function updateConversion() {
-  if (fromCurrencySelect.value === toCurrencySelect.value){
-    alert("con not convert to the same currency");
+function updateConversion () {
+  if (fromCurrencySelect.value === toCurrencySelect.value) {
+    alert('con not convert to the same currency')
     return
   }
-  const amount = parseFloat(amountInput.value) || 0;
-  const fromCurrency = fromCurrencySelect.value;
-  const toCurrency = toCurrencySelect.value;
+  const amount = parseFloat(amountInput.value) || 0
+  const fromCurrency = fromCurrencySelect.value
+  const toCurrency = toCurrencySelect.value
   // console.log (amount2);
 
   // calculate a converted value
-  const exchangeRate = exchangeRates[fromCurrency] / exchangeRates[toCurrency];
-  const converted = (amount * exchangeRate).toFixed(2);
-  convertedAmount.value = converted;
+  const exchangeRate = exchangeRates[fromCurrency] / exchangeRates[toCurrency]
+  const converted = (amount * exchangeRate).toFixed(2)
+  convertedAmount.value = converted
 }
-
-
-
