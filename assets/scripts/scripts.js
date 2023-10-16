f1b1120fee231e967a9ab12fd3f78b4d0b21f2e9
dorinengabdoh <ngabdohdorine@gmail.com>
2023-10-16 09:38:16 -0400

    making my code to be reactif

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,28 +1,34 @@
-const amountInput = document.getElementById('amount')
-const fromCurrencySelect = document.getElementById('from')
-const toCurrencySelect = document.getElementById('to')
-const convertedAmount = document.getElementById('converted-amount')
+const amountInput = document.getElementById("amount");
+const fromCurrencySelect = document.getElementById("from");
+const toCurrencySelect = document.getElementById("to");
+const convertedAmount = document.getElementById("converted-amount");
 
-amountInput.addEventListener('input', updateConversion)
-convertedAmount.addEventListener('ouput', updateConversion)
-fromCurrencySelect.addEventListener('change', updateConversion)
-toCurrencySelect.addEventListener('change', updateConversion)
+amountInput.addEventListener("input", updateConversion);
+fromCurrencySelect.addEventListener("change", updateConversion);
+toCurrencySelect.addEventListener("change", updateConversion);
+convertedAmount.addEventListener("input", (e) => {
+ const int = e.target.value
+ console.log(int);
+ updateConversion()
+});
 
-function updateConversion () {
-  const amount = parseFloat(amountInput.value) || 0
-  // const toamount = parseFloat(convertedAmount.value) || 0
-  const fromCurrency = fromCurrencySelect.value
-  const toCurrency = toCurrencySelect.value
+
+function updateConversion() {
+  const amount = parseFloat(amountInput.value) || 0;
+  const fromCurrency = fromCurrencySelect.value;
+  const toCurrency = toCurrencySelect.value;
+  const amount2 = parseFloat(convertedAmount.value) ||0;
+  console.log (amount2);
 
   // Replace with your own exchange rate logic here
   const exchangeRates = {
     usd: 1,
     eur: 0.85,
-    gbp: 0.72
-  }
+    gbp: 0.72,
+    yen: 149.4,
+  };
 
-  const exchangeRate = exchangeRates[fromCurrency] / exchangeRates[toCurrency]
-  const converted = (amount * exchangeRate).toFixed(2)
-  // const convert = (toamount * exchangeRate).toFixed(2)
-  convertedAmount.value = converted
+  const exchangeRate = exchangeRates[fromCurrency] / exchangeRates[toCurrency];
+  const converted = (amount * exchangeRate).toFixed(2);
+  convertedAmount.value = converted;
 }
