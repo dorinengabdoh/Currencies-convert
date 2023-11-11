ce79646f451b66362c4937ae3dbdbc55c8a0242b
dorinengabdoh <ngabdohdorine@gmail.com>
2023-11-11 09:36:35 -0500

    event listener adding

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,9 +3,17 @@ const fromCurrencySelect = document.getElementById('from')
 const toCurrencySelect = document.getElementById('to')
 const convertedAmount = document.getElementById('converted-amount')
 
-amountInput.addEventListener('input', updateConversion)
+amountInput.addEventListener('input', () => {
+  const amount1 = parseFloat(amountInput.value) || 0
+  const exchangeRate3 = exchangeRates[fromCurrencySelect.value] / exchangeRates[toCurrencySelect.value]
+  const convert3 = (amount1 * exchangeRate3).toFixed(2)
+  console.log(convert3);
+  convertedAmount.value = convert3
+  return convert3,
+  updateSecondConverter()
+})
 fromCurrencySelect.addEventListener('change', updateConversion)
-toCurrencySelect.addEventListener('change', updateConversion)
+toCurrencySelect.addEventListener('change', updateSecondConverter)
 convertedAmount.addEventListener('input', () => {
   const amount2 = parseFloat(convertedAmount.value) || 0
   const exchangeRate2 = exchangeRates[toCurrencySelect.value] / exchangeRates[fromCurrencySelect.value]
@@ -21,7 +29,6 @@ const exchangeRates = {
   yen: 149.4
 }
 
-  let converted;
 function updateConversion () {
   if (fromCurrencySelect.value === toCurrencySelect.value) {
     alert('con not convert to the same currency')
@@ -31,13 +38,10 @@ function updateConversion () {
   console.log(amount);
   const fromCurrency = fromCurrencySelect.value
   const toCurrency = toCurrencySelect.value
-
   const exchangeRate = exchangeRates[fromCurrency] / exchangeRates[toCurrency]
-   converted = (amount * exchangeRate).toFixed(2)
+  const converted = (amount * exchangeRate).toFixed(2)
   convertedAmount.value = converted;
 }
-console.log(converted);
-
 
 function updateSecondConverter(){
   if (fromCurrencySelect.value === toCurrencySelect.value) {
@@ -46,5 +50,9 @@ function updateSecondConverter(){
   }
   const toAmount = parseFloat(convertedAmount.value) || 0  
   console.log(toAmount);
-
+  const fromCurrency = fromCurrencySelect.value
+  const toCurrency = toCurrencySelect.value
+  const exchangeRate = exchangeRates[toCurrency] / exchangeRates[fromCurrency]
+  const convert = (toAmount * exchangeRate).toFixed(2)
+  amountInput.value = convert;
 }
